a095cf35d42ad0967a7ddc40ea787baecc0ae0c0
Karyatis <anna.p212@gmail.com>
2017-05-10 17:09:52 -0500

    Parsing module works

--- fi_data.rb
+++ fi_data.rb
@@ -3,12 +3,12 @@ class FIData
 attr_reader :name, :risk, :zipcode, :date, :results, :violations
 
   def initialize(args={})
-    @name = args[:DBA_Name]
-    @risk = args[:Risk]
-    @zipcode = args[:Zip]
-    @date = args[:Inspection_Date]
-    @results = args[:Results]
-    @violations = args[:Violations]
+    @name = args[:dba_name]
+    @risk = args[:risk]
+    @zipcode = args[:zip]
+    @date = args[:inspection_date]
+    @results = args[:results]
+    @violations = args[:violations]
   end
 end
 
--- spec/fi_data_spec.rb
+++ spec/fi_data_spec.rb
@@ -2,7 +2,7 @@ require_relative '../fi_data'
 
 describe FIData do
 
-  let(:establishment) { FIData.new({DBA_Name: "Konventional Kitchens", Risk: "Risk 3 (Low)", Zip: "60647", Inspection_Date: "05/08/2017", Results: "Pass", Violations: "NO EVIDENCE OF RODENT OR INSECT OUTER OPENINGS PROTECTED/RODENT PROOFED, A WRITTEN LOG SHALL BE MAINTAINED AVAILABLE TO THE INSPECTORS"}) }
+  let(:establishment) { FIData.new({dba_name: "Konventional Kitchens", risk: "Risk 3 (Low)", zipcode: "60647", date: "05/08/2017", results: "Pass", violations: "NO EVIDENCE OF RODENT OR INSECT OUTER OPENINGS PROTECTED/RODENT PROOFED, A WRITTEN LOG SHALL BE MAINTAINED AVAILABLE TO THE INSPECTORS"}) }
 
   it 'returns name of establishment' do
     expect(establishment.name).to eq "Konventional Kitchens"
