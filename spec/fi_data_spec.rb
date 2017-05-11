a6100802d24883bd1c1a295b392afa7af146c2c5
Karyatis <anna.p212@gmail.com>
2017-05-10 19:21:30 -0500

    fidata model tests passing

--- spec/fi_data_spec.rb
+++ spec/fi_data_spec.rb
@@ -7,4 +7,24 @@ describe FIData do
   it 'returns name of establishment' do
     expect(establishment.name).to eq "Konventional Kitchens"
   end
+
+  it 'returns risk level of establishment' do
+    expect(establishment.risk).to eq "Risk 3 (Low)"
+  end
+
+  it 'returns zip code of establishment' do
+    expect(establishment.zipcode).eql?("60647")
+  end
+
+  it 'returns date of establishment inspection' do
+    expect(establishment.date).eql?("05/08/2017")
+  end
+
+  it 'returns result of establishment inspection' do
+    expect(establishment.results).to eq "Pass"
+  end
+
+  it 'returns comments for establishment inspection' do
+    expect(establishment.violations).to eq "NO EVIDENCE OF RODENT OR INSECT OUTER OPENINGS PROTECTED/RODENT PROOFED, A WRITTEN LOG SHALL BE MAINTAINED AVAILABLE TO THE INSPECTORS"
+  end
 end
