1243ecd23f933aad2c9eabb9e9a4428fd5084f84
Anna Pougas <anna.p212@gmail.com>
2017-05-15 11:50:49 -0500

    Update write_story.rb

--- write_story.rb
+++ write_story.rb
@@ -1,6 +1,6 @@
 require_relative 'fi_data'
 require_relative 'fi_data_loader'
-require_relative 'establishments'
+require_relative 'establishment'
 
 	include FIDataLoader
 
@@ -8,10 +8,10 @@ zip_code = ARGV.first
 filepath = ARGV.last
 
 fi_data = FIDataLoader.parse(filepath)
-establishments = Establishments.new(fi_data: fi_data)
+establishment = Establishments.new(fi_data: fi_data)
 
 if fi_data.any? {|establishment| establishment.zipcode.include?(zip_code)}
-	establishments.to_pretty_string(zip_code)
+	establishment.to_pretty_string(zip_code)
 else
-	establishments.zipcode_fail_msg
+	establishment.zipcode_fail_msg
 end
